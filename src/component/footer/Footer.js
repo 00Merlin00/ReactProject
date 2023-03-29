53d3ff85fb1059a4eeee9538e5084bdd98cbbc8b
Mohit Rautela <mrautela12@gmail.com>
2023-03-29 15:03:30 +0530

    some commit

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>02021, Movie, Inc. or its affiliates</div>
+    </div>
+  );
 };
 
 export default Footer;
