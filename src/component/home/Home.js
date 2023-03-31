1631a8dc3255156320d1fb0dae199b12c5248f2c
Mohit Rautela <mrautela12@gmail.com>
2023-03-31 23:18:22 +0530

    some commit

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../movieListing/MovieListing";
+import MovieApi from "../../common/apis/MovieApi";
+import { ApiKey } from "../../common/apis/MovieApiKey";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText = "fanny";
+    const fetchMovies = async () => {
+      const responce = await MovieApi.get(
+        `?apiKey = ${ApiKey}&s=${movieText} & type=movie `
+      ).catch((err) => {
+        console.log("Err:", err);
+      });
+      console.log("The responce from API : ", responce);
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
