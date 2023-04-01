df070388c8730e88e96b142d7e145686e1a39454
Mohit Rautela <mrautela12@gmail.com>
2023-04-01 17:37:57 +0530

    some commit

--- src/common/apis/MovieApiKey.js
+++ src/common/apis/MovieApiKey.js
@@ -1,1 +1,1 @@
-export const ApiKey = " 9ac34b9a";
+export const ApiKey = "9ac34b9a";
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -2,23 +2,28 @@ import React, { useEffect } from "react";
 import MovieListing from "../movieListing/MovieListing";
 import MovieApi from "../../common/apis/MovieApi";
 import { ApiKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
+  const movieText = "fanny";
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const movieText = "fanny";
     const fetchMovies = async () => {
-      const responce = await MovieApi.get(
-        `?apiKey = ${ApiKey}&s=${movieText} & type=movie `
+      const response = await MovieApi.get(
+        `?apiKey=${ApiKey}&s=${movieText}&type=movie `
       ).catch((err) => {
         console.log("Err:", err);
       });
-      console.log("The responce from API : ", responce);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
-  }, []);
+  }, [dispatch]);
   return (
     <div>
-      <div className="banner-im"></div>
+      <div className="banner-img"></div>
       <MovieListing />
     </div>
   );
