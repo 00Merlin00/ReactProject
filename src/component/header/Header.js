0ec9b82aa350dea8c19c4939e0f1e5db509bc0ae
Mohit Rautela <mrautela12@gmail.com>
2023-04-05 00:02:44 +0530

    some commit

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
   const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
-    if (term === "") return alert("Please enter search term!");
+    if (term === "") return alert("Please enter search termp!");
     dispatch(fetchAsyncMovies(term));
     dispatch(fetchAsyncShows(term));
     setTerm("");
