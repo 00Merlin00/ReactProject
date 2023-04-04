597f13798b949fb8670262c7bb97403d5ea652de
Mohit Rautela <mrautela12@gmail.com>
2023-04-04 20:22:54 +0530

    some commit

--- src/component/movieDetail/MovieDetail.js
+++ src/component/movieDetail/MovieDetail.js
@@ -1,34 +1,41 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import "./MovieDetail.scss";
 import { fetchAsyncMovieOrShowDetails } from "../../features/movies/movieSlice";
-import { getSelectedMovieOrShow } from "../../features/movies/movieSlice";
+import {
+  getSelectedMovieOrShow,
+  removeSelectedMovieOrShow,
+} from "../../features/movies/movieSlice";
 
 const MovieDetail = () => {
   const { imdbID } = useParams();
 
-  const dispatach = useDispatch();
+  const dispatch = useDispatch();
   const data = useSelector(getSelectedMovieOrShow);
   console.log(data);
   useEffect(() => {
-    dispatach(fetchAsyncMovieOrShowDetails(imdbID));
-  }, [dispatach, imdbID]);
+    dispatch(fetchAsyncMovieOrShowDetails(imdbID));
+    return () => {
+      dispatch(removeSelectedMovieOrShow());
+    };
+  }, [dispatch, imdbID]);
   return (
     <div className="movie-section">
       <div className="section-left">
         <div className="movie-title">{data.Title}</div>
         <div className="movie-rating">
           <span>
-            IMDB Rating <i className="fa fa-star"></i>: {data.imdbRating}
+            IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
           </span>
           <span>
-            IMDB Votes <i className="fa fa-thumbs-up"></i>: {data.imdbVotes}
+            IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
           </span>
           <span>
-            Runtime <i className="fa fa-film"></i>: {data.Runtime}
+            Runtime <i className="fa fa-film"></i> : {data.Runtime}
           </span>
           <span>
-            Year <i className="fa fa-calendar"></i>: {data.Year}
+            Year <i className="fa fa-calendar"></i> : {data.Year}
           </span>
         </div>
         <div className="movie-plot">{data.Plot}</div>
